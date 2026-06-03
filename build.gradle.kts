4e064af5aa465bc0167cd6ee4ac559850f933101
jani681 <jani681@gmail.com>
2026-06-03 12:07:06 +0500

    Migrate Google Services plugin to plugins block

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.google.gms:google-services:4.4.2")
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.google.devtools.ksp) apply false
     alias(libs.plugins.roborazzi) apply false
     alias(libs.plugins.secrets) apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
